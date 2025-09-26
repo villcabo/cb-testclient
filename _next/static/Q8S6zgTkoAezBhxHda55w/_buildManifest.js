0951849badc42ea9b7d15adb2fd1d6dd65c31cf3
villcabo <villcabo@users.noreply.github.com>
2025-09-26 18:58:11 +0000

    deploy: 38e23544aaedd62290d81c0af44779952af5defc

---
+++ _next/static/Q8S6zgTkoAezBhxHda55w/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/_error":["static/chunks/pages/_error-5f46dcfa5b6f6b58.js"],sortedPages:["/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
