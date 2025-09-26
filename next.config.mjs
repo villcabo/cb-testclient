483165907e8037ceadcd20a04867d06e90ba3436
Bismarck Villca <bismarck.villca@gmail.com>
2025-09-26 15:23:41 -0400

    fix: update GitHub Pages flag to use environment variable
    Signed-off-by: Bismarck Villca <bismarck.villca@gmail.com>

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const isGithubPages = 'true';
+const isGithubPages = process.env.GITHUB_PAGES == 'true';
 const nextConfig = {
   output: 'export',
   basePath: isGithubPages ? '/cb-testclient' : '',
